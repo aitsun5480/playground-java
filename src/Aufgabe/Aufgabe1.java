e89f6496e8b7b6fcb3a8232c0d5dfa3cfd849a59
aitsun5480 <shio.tomate@gmail.com>
2022-01-02 21:34:17 +0100

    aufgabe

---
+++ src/Aufgabe/Aufgabe1.java
@@ -0,0 +1,30 @@
+package Aufgabe;
+
+import Werkzeug.Drucker;
+
+// Aufgabe:  Drucke die Zahlen 1 bis 10, jeweils auf einer eigenen Zeile.
+//タスク：1から10までの数字をそれぞれ別々の行に印刷します。
+public class Aufgabe1 {
+    public static void main(String[] args) {
+        Drucker.neueZeileDrucken();
+        Drucker.drucken("1");
+        Drucker.neueZeileDrucken();
+        Drucker.drucken("2");
+        Drucker.neueZeileDrucken();
+        Drucker.drucken("3");
+        Drucker.neueZeileDrucken();
+        Drucker.drucken("4");
+        Drucker.neueZeileDrucken();
+        Drucker.drucken("5");
+        Drucker.neueZeileDrucken();
+        Drucker.drucken("6");
+        Drucker.neueZeileDrucken();
+        Drucker.drucken("7");
+        Drucker.neueZeileDrucken();
+        Drucker.drucken("8");
+        Drucker.neueZeileDrucken();
+        Drucker.drucken("9");
+        Drucker.neueZeileDrucken();
+        Drucker.drucken("10");
+    }
+}
---
+++ src/Werkzeug/Drucker.java
@@ -0,0 +1,11 @@
+package Werkzeug;
+
+public class Drucker {
+    public static void drucken(String text) {
+        System.out.print(text);
+    }
+
+    public static void neueZeileDrucken() {
+        System.out.println();
+    }
+}
