28b1a60ff0950f09e92ae56e4a5d09ab921a826f
aitsun5480 <shio.tomate@gmail.com>
2022-01-02 22:00:42 +0100

    Aufgabe2

---
+++ src/Aufgabe/Aufgabe2.java
@@ -0,0 +1,14 @@
+package Aufgabe;
+
+import Werkzeug.Drucker;
+
+//Aufgabe 2: Drucke die Zahlen 1 - 1000
+//演習2：1から1000までの数字を印刷します
+public class Aufgabe2 {
+    public static void main(String[] args) {
+        for (int zähler = 1; zähler <= 1000; zähler++) {
+            Drucker.neueZeileDrucken();
+            Drucker.zahlDrucken(zähler);
+        }
+    }
+}
--- src/Werkzeug/Drucker.java
+++ src/Werkzeug/Drucker.java
@@ -5,6 +5,11 @@ public class Drucker {
         System.out.print(text);
     }
 
+    public static void zahlDrucken(int zahl) {
+        System.out.print(String.valueOf(zahl));
+    }
+
+
     public static void neueZeileDrucken() {
         System.out.println();
     }
