ca4c4dc565a81fd9c1808de47d5f7e37d6c5ed86
aitsun5480 <shio.tomate@gmail.com>
2022-01-08 21:47:28 +0100

    Daniels Refactoring

--- src/Aufgabe/Aufgabe7.java
+++ src/Aufgabe/Aufgabe7.java
@@ -3,51 +3,28 @@ package Aufgabe;
 import Werkzeug.Farbe;
 import Werkzeug.Zeichner;
 
+import java.awt.*;
+
 //Aufgabe 7: Zeichne die deutsche Flagge
 public class Aufgabe7 {
 
     public static void main(String[] args) {
-        skizziereSchwarzenBalken();
-        skizziereGoldenenBalken();
-        skizziereRoteBalken();
+        skizziereBalken(0, 30,Farbe.SCHWARZ);
+        skizziereBalken(30,60,Farbe.ROT);
+        skizziereBalken(60,90,Farbe.GOLD);
 
         Zeichner.zeichnen();
     }
 
-    public static void skizziereSchwarzenBalken() {
-        for (int y = 0; y < 30; y++) {
-            skizziereSchwarzeReihe(y);
+    public static void skizziereBalken(int minY, int maxY, Color Farbe) {
+        for (int y = minY; y < maxY; y++) {
+            skizziereReihe(y, Farbe);
         }
     }
 
-    private static void skizziereSchwarzeReihe(int y) {
+    private static void skizziereReihe(int y, Color farbe) {
         for (int x = 0; x < 90; x++) {
-            Zeichner.pixelSkizzieren(x, y, Farbe.SCHWARZ);
-        }
-    }
-    public static void skizziereRoteBalken() {
-        for (int y = 30; y < 60; y++) {
-            skizziereRoteReihe(y);
-        }
-    }
-
-    private static void skizziereRoteReihe(int y) {
-        for (int x = 0; x < 90; x++) {
-            Zeichner.pixelSkizzieren(x, y, Farbe.ROT);
-        }
-    }
-
-
-    public static void skizziereGoldenenBalken() {
-        for (int y = 60; y < 90; y++) {
-            skizziereGoldeneReihe(y);
-        }
-    }
-
-    private static void skizziereGoldeneReihe(int y) {
-        for (int x = 0; x < 90; x++) {
-            Zeichner.pixelSkizzieren(x, y, Farbe.GOLD);
+            Zeichner.pixelSkizzieren(x, y, farbe);
         }
     }
 }
-
