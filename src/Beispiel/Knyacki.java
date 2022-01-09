bdd854ffb50b33e8d3a24f89a1b1d4ee8acb93df
Daniel Schulz <daniel.schulz@workmotion.com>
2022-01-09 22:54:33 +0100

    Knyacki game für 2 Spieler

--- src/Beispiel/Knyacki.java
+++ src/Beispiel/Knyacki.java
@@ -12,57 +12,86 @@ import java.awt.*;
 
 public class Knyacki {
     private static Position position = new Position(50, 50);
+    private static Richtung richtung = Richtung.OBEN;
+
     private static final Color KOPF_FARBE = Farbe.GOLD;
     private static final Color KÖRPER_FARBE = Farbe.GRÜN;
     private static final Color WAND_FARBE = Farbe.ROT;
 
-    public static void main(String[] args) {
-        Tastatur.wennTaste(Taste.LINKS, Knyacki::linksBewegen);
-        Tastatur.wennTaste(Taste.RECHTS, Knyacki::rechtsBewegen);
-        Tastatur.wennTaste(Taste.OBEN, Knyacki::obenBewegen);
-        Tastatur.wennTaste(Taste.UNTEN, Knyacki::untenBewegen);
-        Maus.wennKlick(Knyacki::telportieren);
+    private enum Richtung {
+        RECHTS, LINKS, OBEN, UNTEN
+    }
 
-        bewegen(position.x, position.y);
+    public static void main(String[] args) {
+        Tastatur.wennTaste(Taste.LINKS, Knyacki::linksDrehen);
+        Tastatur.wennTaste(Taste.RECHTS, Knyacki::rechtsDrehen);
+
+        bewegen();
 
         while (true) {
             if (Zeichner.pixelLesen(Maus.position().x, Maus.position().y) == KÖRPER_FARBE) {
                 Zeichner.pixelSkizzieren(Maus.position().x, Maus.position().y, Farbe.ROT);
             }
+            bewegen();
             Zeichner.zeichnen();
-            Zeit.warten(13);
+            Zeit.warten(250);
         }
     }
 
-    public static void telportieren() {
-        Zeichner.pixelSkizzieren(position.x, position.y, null);
-
-        position = Maus.position();
-
-        Zeichner.pixelSkizzieren(position.x, position.y, KOPF_FARBE);
-
+    public static void linksDrehen() {
+        switch (richtung) {
+            case OBEN:
+                richtung = Richtung.LINKS;
+                break;
+            case RECHTS:
+                richtung = Richtung.OBEN;
+                break;
+            case UNTEN:
+                richtung = Richtung.RECHTS;
+                break;
+            case LINKS:
+                richtung = Richtung.UNTEN;
+                break;
+        }
     }
 
-    public static void linksBewegen() {
-        bewegen(position.x - 1, position.y);
+    public static void rechtsDrehen() {
+        switch (richtung) {
+            case OBEN:
+                richtung = Richtung.RECHTS;
+                break;
+            case RECHTS:
+                richtung = Richtung.UNTEN;
+                break;
+            case UNTEN:
+                richtung = Richtung.LINKS;
+                break;
+            case LINKS:
+                richtung = Richtung.OBEN;
+                break;
+        }
     }
 
-    public static void rechtsBewegen() {
-        bewegen(position.x + 1, position.y);
+    public static void bewegen() {
+        int neuePosX = position.x;
+        int neuePosY = position.y;
 
-    }
+        switch (richtung) {
+            case OBEN:
+                neuePosY--;
+                break;
+            case RECHTS:
+                neuePosX++;
+                break;
+            case UNTEN:
+                neuePosY++;
+                break;
+            case LINKS:
+                neuePosX--;
+                break;
+        }
 
-    public static void obenBewegen() {
-        bewegen(position.x, position.y - 1);
-
-    }
-
-    public static void untenBewegen() {
-        bewegen(position.x, position.y + 1);
-    }
-
-    public static void bewegen(int neuePosX, int neuePosY) {
-        if (Zeichner.pixelLesen(neuePosX, neuePosY) != WAND_FARBE) {
+        if (Zeichner.pixelLesen(neuePosX, neuePosY) != WAND_FARBE && neuePosX > 0 && neuePosX < Zeichner.PIXEL_BREITE&& neuePosY > 0 && neuePosY < Zeichner.PIXEL_BREITE) {
             Zeichner.pixelSkizzieren(position.x, position.y, KÖRPER_FARBE);
             position.x = neuePosX;
             position.y = neuePosY;
