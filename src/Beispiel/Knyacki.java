c8895148cb62fcc8b23589d45efda2827c4ecfef
Daniel Schulz <daniel.schulz@workmotion.com>
2022-01-09 22:37:50 +0100

    Mauer hinzufügen

--- src/Beispiel/Knyacki.java
+++ src/Beispiel/Knyacki.java
@@ -12,31 +12,26 @@ import java.awt.*;
 
 public class Knyacki {
     private static Position position = new Position(50, 50);
-    private static boolean löschtGerade = false;
     private static final Color KOPF_FARBE = Farbe.GOLD;
     private static final Color KÖRPER_FARBE = Farbe.GRÜN;
+    private static final Color WAND_FARBE = Farbe.ROT;
 
     public static void main(String[] args) {
         Tastatur.wennTaste(Taste.LINKS, Knyacki::linksBewegen);
         Tastatur.wennTaste(Taste.RECHTS, Knyacki::rechtsBewegen);
         Tastatur.wennTaste(Taste.OBEN, Knyacki::obenBewegen);
         Tastatur.wennTaste(Taste.UNTEN, Knyacki::untenBewegen);
-        Tastatur.wennTaste(Taste.SPACE, Knyacki::löschenAktivieren);
         Maus.wennKlick(Knyacki::telportieren);
 
+        bewegen(position.x, position.y);
+
         while (true) {
-            if (löschtGerade && Zeichner.pixelLesen(Maus.position().x,Maus.position().y) == KÖRPER_FARBE) {
-                position = Maus.position();
-                Zeichner.pixelSkizzieren(position.x, position.y, null);
+            if (Zeichner.pixelLesen(Maus.position().x, Maus.position().y) == KÖRPER_FARBE) {
+                Zeichner.pixelSkizzieren(Maus.position().x, Maus.position().y, Farbe.ROT);
             }
             Zeichner.zeichnen();
             Zeit.warten(13);
         }
-    }
-
-    private static void löschenAktivieren() {
-        Zeichner.pixelSkizzieren(position.x, position.y, null);
-        löschtGerade = !löschtGerade;
     }
 
     public static void telportieren() {
@@ -67,7 +62,7 @@ public class Knyacki {
     }
 
     public static void bewegen(int neuePosX, int neuePosY) {
-        if (!löschtGerade && Zeichner.pixelLesen(neuePosX, neuePosY) == null) {
+        if (Zeichner.pixelLesen(neuePosX, neuePosY) != WAND_FARBE) {
             Zeichner.pixelSkizzieren(position.x, position.y, KÖRPER_FARBE);
             position.x = neuePosX;
             position.y = neuePosY;
