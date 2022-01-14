a8b45457ab7430e406861ec6c81ec5f76032fa67
Daniel Schulz <daniel.schulz@workmotion.com>
2022-01-14 21:42:53 +0100

    Lautsprecher optimieren

--- src/Werkzeug/Lautsprecher.java
+++ src/Werkzeug/Lautsprecher.java
@@ -5,20 +5,29 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Lautsprecher {
+    private static final Map<String, Clip> sounds = new HashMap<>();
+
     public static void abspielen(String soundName) {
         abspielen(soundName, 0);
     }
 
     public static void abspielen(String soundName, int lautStärke) {
         try {
-            String path = System.getProperty("user.dir") + "/assets/sounds/" + soundName + ".wav";
-            AudioInputStream audioIn = AudioSystem.getAudioInputStream(new URL("file://" + path));
+            if (!sounds.containsKey(soundName)) {
+                String path = System.getProperty("user.dir") + "/assets/sounds/" + soundName + ".wav";
+                AudioInputStream audioIn = AudioSystem.getAudioInputStream(new URL("file://" + path));
 
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioIn);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioIn);
 
+                sounds.put(soundName, clip);
+            }
+            Clip clip = sounds.get(soundName);
+            clip.setFramePosition(0);
             FloatControl gainControl =
                     (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
             gainControl.setValue(lautStärke);
